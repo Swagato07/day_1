543b3438f357afd17a77e0f5182111faf583f4c6
Swagato Chatterjee <swagatochatterjee07@gmail.com>
2022-06-01 11:39:26 +0530

    day 1 commit(margins and padding

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.blue,
+            child: Container(
+              margin: EdgeInsets.all(5),
+              //padding: EdgeInsets.all(5),
+              color: Colors.orange,
+              child: Container(
+                margin: EdgeInsets.all(5),
+                child: Text("Hello World"),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
